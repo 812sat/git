50709cd2431abd38850512e77797d017c29bd4c9
Gulnur Satkylova <812sat@gmail.com>
2023-05-27 18:12:55 +0600

    First commit

---
+++ git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git
+//
+//  Created by W1! on 27/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Edit")
+    }
+
+
+}
+
